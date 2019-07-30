47d73ef40deed860b7b3a0aafffdae1ae3db7bf1
Nadiane Torres França Ferreira <nadiane.t.ferreira@accenture.com>
2019-07-30 19:00:25 -0300

    Adicionando Configuração Swagger e ajustes nos nomes dos endpoints.

---
+++ src/main/java/br/com/compasso/web/cadastroclientes/CadastroClientesApplication.java
@@ -0,0 +1,15 @@
+package br.com.compasso.web.cadastroclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class CadastroClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroClientesApplication.class, args);
+	}
+
+}
